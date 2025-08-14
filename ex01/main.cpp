ef5e29d2e79c781fe0b09a7749519e42b8497656
Nofy Leclercq <noufy33@yahoo.com>
2025-08-14 15:20:27 +0200

    ex01 is done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 09:21:15 by nolecler          #+#    #+#             */
-/*   Updated: 2025/08/14 14:15:39 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/08/14 15:16:44 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,45 +17,23 @@
 #include "Dog.hpp"
 #include "Brain.hpp"
 
-// a garder wrongAnimal??
-
-// int main()
-// {
-    
-//     const Animal* j = new Dog();
-//     std::cout << std::endl;
-    
-//     const Animal* i = new Cat();
-//     std::cout << std::endl;
-//     std::cout << i->getType() << std::endl;
-    
-//     i->makeSound();
-//     j->makeSound();
-
-//     delete i;
-//     delete j;
-    
-//     return 0;
-// }
 
 int main()
 {
     const int N = 10;
     Animal* animals[N];  // 1. Tableau de pointeurs Animal*
 
-    // 2. Remplir moitié Dog, moitié Cat
-    for (int i = 0; i < N / 2; ++i)
-        animals[i] = new Dog();
-    for (int i = N / 2; i < N; ++i)
-        animals[i] = new Cat();
-
-    // 3. Tester avec makeSound() (polymorphisme)
+    for (int i = 0; i < 10; ++i)
+    {
+        if (i < 5)
+            animals[i] = new Dog();
+        else
+            animals[i] = new Cat();
+    }
     for (int i = 0; i < N; ++i)
         animals[i]->makeSound();
-
-    // 4. Supprimer chaque Animal via pointeur base
     for (int i = 0; i < N; ++i)
         delete animals[i];
 
-    return 0;
+    return (0);
 }
